70f0ebddaf45efcbd15a08cfac5440c53f25ffcd
bestgopher <84328409@qq.com>
2021-12-11 21:59:40 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,3 @@ pub use crate::db::MiniDB;
 pub use crate::error::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
